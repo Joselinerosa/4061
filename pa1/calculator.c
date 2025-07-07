1bd9d8717fef567a94e94f39a0241797a7ae7288
PTregembo <tregembop@gmail.com>
2025-07-07 17:38:43 -0500

    finished task 4 for phase 2

--- pa1/calculator.c
+++ pa1/calculator.c
@@ -1,33 +1,15 @@
 #include "calculator.h"
 
+typedef struct {
+    int count;
+    long sum;
+    char filename[256];
+} PipeMessage;
+
 /* Function prototypes */
 /* Feel free to add your own helper functions */
-void process_directory(const char *dir_path);
+
 int count_files_in_directory(const char *dir_path);
-
-int main(int argc, char *argv[]) {
-    /* TODO: Task 1 - Argument parsing */
-    //Joseline's code
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s PATH_TO_DIR\n", argv[0]);
-        exit(1);
-    }
-
-    const char *dir_path = argv[1];
-    /* TODO: Get the path to the directory from command line options */ 
-
-    DIR *dir = opendir(dir_path);
-    if (dir==NULL){
-        printf("ERROR: %s directory does not exist\n", dir_path);
-        return -1;
-    }
-    closedir(dir);
-    /* TODO: Process the directory */
-    process_directory(dir_path);
-
-    return 0;
-}
-
 
 
 void process_directory(const char *dir_path) {
@@ -52,6 +34,9 @@ void process_directory(const char *dir_path) {
     }
 
     pid_t pids[num_files];
+    int pipes[num_files][2]; //pipes for communication 
+    //pipes[i][0]= read end
+    //pipes[i][1] = write end 
     int file_index = 0;
 
 // Reset directory stream so we can read files again
@@ -62,6 +47,11 @@ void process_directory(const char *dir_path) {
     while((entry = readdir(dir)) != NULL) {
         if((strcmp(entry->d_name, ".") == 0) || (strcmp(entry->d_name, "..") == 0))
             continue;
+
+        if (pipe(pipes[file_index])==-1){
+            perror("pipe");
+            exit(1);
+        }
 //Peyton's Code
         char filepath[1024];
         snprintf(filepath, sizeof(filepath), "%s/%s", dir_path, entry->d_name);
@@ -72,21 +62,54 @@ void process_directory(const char *dir_path) {
             exit(1);
         } else if (pid == 0) {
             // Child process exec's child program
-            execl("./child", "child", filepath, NULL);
+            close(pipes[file_index][0]); //close the READ END in child
+            char write_fd_str[10]; //convert write fd to string for argv
+            snprintf(write_fd_str, sizeof(write_fd_str), "%d", pipes[file_index][1]);
+
+            
+            execl("./child", "child", filepath, write_fd_str, NULL);
             perror("execl");  // exec only returns on error
             exit(1);
         } else {
             // Parent saves child pid
-            pids[file_index++] = pid;
+            pids[file_index] = pid;
+            close(pipes[file_index][1]); // close write end in parent
+            file_index++;
         }
     }
     closedir(dir);
+
+    long total_sum=0;
+    int total_count=0;
+
+    for (int i = 0; i < file_index; i++) {
+        PipeMessage msg;
+        ssize_t bytes_read = read(pipes[i][0], &msg, sizeof(PipeMessage));
+        if (bytes_read != sizeof(PipeMessage)) {
+            perror("read");
+            exit(1);
+        }
+        printf("%s: %d, %ld\n", msg.filename, msg.count, msg.sum);
+        total_sum += msg.sum;
+        total_count += msg.count;
+        close(pipes[i][0]);
+    }
+
 
     // Wait for all children to finish
     for (int i = 0; i < file_index; i++) {
         int status;
         waitpid(pids[i], &status, 0);
     }
+
+    // Print total sum and average
+    printf("Sum: %ld\n", total_sum);
+    if (total_count > 0) {
+        printf("Average: %.2f\n", (double)total_sum / total_count);
+    } else {
+        printf("Average: 0.00\n");
+    }
+}
 
 /* Count files first to allocate memory */
 //int num_files = count_files_in_directory(dir_path);
@@ -105,7 +128,7 @@ void process_directory(const char *dir_path) {
 /* Calculate and print final results */
 
 /* TODO: Remember to deallocate any memory */
-}
+
 
 int count_files_in_directory(const char *dir_path) {
     int count = 0;
@@ -125,5 +148,28 @@ int count_files_in_directory(const char *dir_path) {
 
     closedir(dir);
     return count;
+}
+
+int main(int argc, char *argv[]) {
+    /* TODO: Task 1 - Argument parsing */
+    //Joseline's code
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s PATH_TO_DIR\n", argv[0]);
+        exit(1);
+    }
+
+    const char *dir_path = argv[1];
+    /* TODO: Get the path to the directory from command line options */ 
+
+    DIR *dir = opendir(dir_path);
+    if (dir==NULL){
+        printf("ERROR: %s directory does not exist\n", dir_path);
+        return -1;
+    }
+    closedir(dir);
+    /* TODO: Process the directory */
+    process_directory(dir_path);
+
+    return 0;
 }
 
